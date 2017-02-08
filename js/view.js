bcd331f835325410e0e7ff1adc66756748057945
VinciXie <szqq602@163.com>
2017-02-08 21:08:15 +0800

    整体布局完成

--- js/view.js
+++ js/view.js
@@ -2,6 +2,18 @@
 
 var winWidth = window.innerWidth//窗口的宽
 var winHeight = window.innerHeight//窗口的高
+var w = e('.w')
+var video = e('video')
 var section = e('section')//播放列表区域
-log('winHeight', winHeight)
+var header = e('header')//播放列表区域
+w.style.cssText = `height:${winHeight}px;`
 section.style.cssText = `height:${winHeight}px;`
+
+window.addEventListener("resize", function() {
+  log("窗口大小改变")
+  var winHeight = window.innerHeight//窗口的高
+  log('winHeight', winHeight)
+  section.style.cssText = `height:${winHeight}px;`
+  w.style.cssText = `height:${winHeight}px;`
+  // header.style.cssText = `width:${winHeight}px;`
+})
